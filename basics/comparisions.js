ba22b777a58ef75c326278598410c4365fbdb590
VIVEK GUPTA <142393054+PyCoderVivek@users.noreply.github.com>
2025-06-23 16:57:51 +0000

    datatypes and comparision operaters

---
+++ basics/comparisions.js
@@ -0,0 +1,12 @@
+console.log(2>4);
+console.log(3<2);
+console.log(2==2);
+
+// these are the basic comparisions 
+// alwyas compare same D.T 
+
+// don't compare undefined and null (only in rare cases)
+// there is some inconsistency in JS language 
+
+
+
---
+++ basics/datatype_conversion_and_operation.js
@@ -0,0 +1,28 @@
+let newNumber = 2
+
+let newStrNumber = String(newNumber)
+console.log(typeof newStrNumber);
+
+// alphanumeric and string values are convertable but it will show the value as NaN but type will number 
+// in Boolean 1=> true ; 0=> false and (empty str)""=> false ; (non empty str)"Vivek"=> true
+
+
+// Type | typeof_return_value | Object_wrapper
+// Null	"object"	N/A
+// Undefined	"undefined"	N/A
+// Boolean	"boolean"	Boolean
+// Number	"number"	Number
+// BigInt	"bigint"	BigInt
+// String	"string"	String
+// Symbol	"symbol"	Symbol
+
+
+
+console.log(2+2);
+console.log("2" + 1 + 1) // in this case the first value or variables D.T will be considered for the other operant 
+let num = 2+2
+console.log(num + "2");
+console.log("2" + num + num);
+
+
+   

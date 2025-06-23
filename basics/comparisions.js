d93f8f2668cbeed15ad31afa94d0590dbb72b4ad
VIVEK GUPTA <142393054+PyCoderVivek@users.noreply.github.com>
2025-06-23 16:58:09 +0000

    comparision operater

--- basics/comparisions.js
+++ basics/comparisions.js
@@ -5,8 +5,9 @@ console.log(2==2);
 // these are the basic comparisions 
 // alwyas compare same D.T 
 
-// don't compare undefined and null (only in rare cases)
-// there is some inconsistency in JS language 
+// avoid compareing undefined and null (only in rare cases) #clean code 
+// there is some inconsistency in JS language in comaparisions concept 
 
+// === is a strict comparisions operater (it checks datatype also and do not convert the D.T during Comaparision)
 
 

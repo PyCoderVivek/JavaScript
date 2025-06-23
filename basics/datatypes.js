7fb5705384e2e42adf8c96b2fd4a8abbc73dfe79
VIVEK GUPTA <142393054+PyCoderVivek@users.noreply.github.com>
2025-06-23 16:04:08 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+let a = "Hii" // string
+let b = 3 // number
+let c = 409809408.0 // bigint
+let d = true // boolean true/false
+let e ; // undefined
+
+/* and many more ,
+symbol --> for uniqueness of component 
+object
+*/
+
